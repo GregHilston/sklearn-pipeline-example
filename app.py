bc0d9ed5b9a6057dbf7446e1b49016d6f0772613
GregHilston <Gregory.Hilston@gmail.com>
2021-07-10 08:02:32 -0400

    Adds tools to improve our code and catch bugs

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import typing
 
 import pandas as pd
 import sklearn
-from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-
+from sklearn.preprocessing import StandardScaler
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -25,13 +24,27 @@ def sklearn_to_df(sklearn_dataset: sklearn.utils.Bunch) -> typing.Tuple[pd.DataF
     return df, target
 
 
-def main():
+def main(random_state):
     # loads our data into independent and dependent variables
     X, y = sklearn_to_df(load_boston())
 
     # splits our data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
+    # separates our columns by numeric and categorical
+    categorical_columns = ["CHAS"]
+    numeric_columns = [column for column in list(X) if column not in categorical_columns]
+
+    # scales our data by only fitting on our training but scaling both training and testing
+    # this avoids data leakage from our test set
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train[X_train[numeric_columns]])
+    X_test = scaler.transform(X_test)
+
+    # trains our model
+    reg = LinearRegression().fit(X, y)
+    print(reg)
+
 
 if __name__ == "__main__":
-    main()
+    main(random_state=random_state)
---
+++ tasks.py
@@ -0,0 +1,38 @@
+"""Invokes tasks"""
+from invoke import task
+
+
+ENTRYPOINT_PATH = "app.py"
+
+
+@task
+def run(ctx):
+    """Runs the application"""
+    ctx.run(f"poetry run python3 {ENTRYPOINT_PATH}", echo=True)
+
+
+@task
+def format(ctx):
+    """Formats Python code"""
+    ctx.run(f"poetry run black {ENTRYPOINT_PATH}", echo=True)
+    ctx.run(f"poetry run isort {ENTRYPOINT_PATH}", echo=True)
+
+
+@task
+def lint(ctx):
+    """Lints Python code"""
+    ctx.run(f"poetry run flake8 --show-source {ENTRYPOINT_PATH}", echo=True)
+    ctx.run(f"poetry run pylint {ENTRYPOINT_PATH}", echo=True)
+
+
+@task
+def type_check(ctx):
+    """Checks types of our Python source code"""
+    ctx.run(f"poetry run mypy {ENTRYPOINT_PATH}", echo=True)
+
+
+@task(pre=[format, lint, type_check])
+def magic(ctx):
+    """Performs all our checking steps: format, lint, type_check."""
+    pass
+
