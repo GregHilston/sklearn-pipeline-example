96e4d0a99a7e9656e58d71bb53de0270741f5cc8
GregHilston <Gregory.Hilston@gmail.com>
2021-07-10 07:41:19 -0400

    Splits our data into train and test splits

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 import pandas as pd
 import sklearn
@@ -14,22 +15,22 @@ logger = logging.getLogger(__name__)
 random_state = 42
 
 
-def sklearn_to_df(sklearn_dataset):
+def sklearn_to_df(sklearn_dataset: sklearn.utils.Bunch) -> typing.Tuple[pd.DataFrame, pd.Series]:
     """Converts an sklearn premade dataset into a Pandas DataFrame
 
-    from: https://stackoverflow.com/a/46379878/1983957
+    based on: https://stackoverflow.com/a/46379878/1983957
     """
     df = pd.DataFrame(sklearn_dataset.data, columns=sklearn_dataset.feature_names)
-    df['target'] = pd.Series(sklearn_dataset.target)
-    return df
+    target = pd.Series(sklearn_dataset.target)
+    return df, target
 
 
 def main():
     # loads our data into independent and dependent variables
-    df = sklearn_to_df(load_boston())
+    X, y = sklearn_to_df(load_boston())
 
     # splits our data
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
 
 if __name__ == "__main__":
