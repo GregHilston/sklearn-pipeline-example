274dc275ce53fb14456374031cea24832bd61912
GregHilston <Gregory.Hilston@gmail.com>
2021-07-11 20:25:28 -0400

    WIP Demonstrates how to compose Pipelines
    Going to remove Pipeline initialization ourselves, so we don't have to
    provide names as they are pretty low value

--- app.py
+++ app.py
@@ -38,15 +38,31 @@ def main(random_state):
     categorical_columns = ["CHAS"]
     numeric_columns = [column for column in list(X) if column not in categorical_columns]
 
-    # trains our model
-    pipe = Pipeline([
+    # We separate out our feature engineering Pipeline from our model Pipeline because we can fit the feature engineering Pipeline now
+    # and then tag on different Model steps later, reusing the original feature_pipe object.
+    feature_pipe = Pipeline([
         ("scaler", StandardScaler(numeric_columns)),
+    ])
+
+    model_pipe = Pipeline([
         ("decision tree regressor", DecisionTreeRegressor(random_state=random_state))
     ])
-    regressor = pipe.fit(X_train, y_train)
+
+    pipe = Pipeline([
+        feature_pipe,
+        model_pipe
+    ])
+
+    # When we call `.fit()` on the Pipeline, we call `.fit()` on EVERY step in the pipeline, sequentially.
+    # When we call `.predict()`, on the Pipeline, we call `.transform` on EVERY step in the pipeline, sequentially, except the last one if the last step extends a Model.
+    # If you have another model, in the middle, this will break the pipeline unless we modify the Sklearn Pipeline code.
+
+    # This fits the `pipe` object, in place, allowing me to pickle/dill the `pipe` object for inference.
+    # trains our model
+    pipe.fit(X_train, y_train)
 
     # evaluates our model
-    logger.info(f"regressor R^2 score {regressor.score(X_test, y_test)}")
+    logger.info(f"regressor R^2 score {pipe.score(X_test, y_test)}")
 
 
 if __name__ == "__main__":
